52e2f55782856935d2e87cbb358d27ac47313c92
“Arpit <111arpit1@gmail.com>
2022-11-15 12:49:55 +0530

    all done

--- EasySharing/settings.py
+++ EasySharing/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIR = {
-    os.path.join(BASE_DIR , "public/static")
+    os.path.join(BASE_DIR , "static")
 }
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
